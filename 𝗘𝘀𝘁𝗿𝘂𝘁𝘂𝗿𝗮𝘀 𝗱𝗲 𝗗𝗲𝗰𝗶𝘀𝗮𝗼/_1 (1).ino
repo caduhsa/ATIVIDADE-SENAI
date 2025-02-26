6f3461828d091c87a579feb9af5d6d1d7474edb8
Cadu <carlos.e.alves23@aluno.senai.br>
2025-02-26 16:34:37 -0300

    Semaforo e cruzamento/Documento sem t\303\255tulo (1).docx

---
+++ 𝗘𝘀𝘁𝗿𝘂𝘁𝘂𝗿𝗮𝘀 𝗱𝗲 𝗗𝗲𝗰𝗶𝘀𝗮𝗼/_1 (1).ino
@@ -0,0 +1,47 @@
+/*
+Nome do Arquivo:𝗘𝘅𝗲𝗿𝗰𝗶𝗰𝗶𝗼𝘀
+Autor:CAH
+Data:26/02/2025
+Descricao:.....
+*/
+float  peso= 78;
+float  altura= 1.75;
+float resultadoReal = 0;
+
+
+void setup()
+{
+  Serial.begin(9600); 
+  resultadoReal = peso / sq(altura);
+  Serial.println(resultadoReal);
+  
+ if (resultadoReal  > 25){
+    Serial.println("Acima do peso");
+ 
+  }else if( resultadoReal >= 40){
+  Serial.println ("Obesidade |||"); 
+   
+   }else if( resultadoReal <= 35.0 && resultadoReal > 39.9){
+  Serial.println ("Obesidade ||"); 
+   
+   }else if( resultadoReal <= 30.0 && resultadoReal > 34.9){
+  Serial.println ("Obesidade |"); 
+   
+   }else if( resultadoReal <= 25.0 && resultadoReal > 29.9){
+  Serial.println ("Excesso de Peso"); 
+   
+   }else if( resultadoReal <= 18.5 && resultadoReal > 24.9){
+  Serial.println ("Peso Normal"); 
+  
+    }else{
+    Serial.println("Abaixo do peso normal");
+  
+  }
+  
+}
+
+void loop()
+{
+  
+  
+}
