0d9ab1f7623eb7f2a3d511b555303f5910c0d7e3
Cadu <carlos.e.alves23@aluno.senai.br>
2025-02-28 15:20:08 -0300

    EXECICIOS 1 AO 8/exercicio_do_1_ao_41.ino

---
+++ EXECICIOS 1 AO 8/exercicio_do_1_ao_41.ino
@@ -0,0 +1,48 @@
+//**EXERCICIO 1***
+int temperatura = 45;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(9, OUTPUT);
+  
+  
+   //EXERCICIO 4 
+  int numero = 1;
+  
+  if(numero > 0){
+    Serial.println("Positivo");
+  }
+   if(numero < 0){
+    Serial.println("Negativo"); 
+  }
+  
+}
+void loop()
+{
+if ( temperatura > 30)
+  {
+   digitalWrite(13, HIGH);
+}
+  
+  //EXERCICIO 2
+  int bateria = 10;
+  if (bateria < 20)
+  {
+    digitalWrite(12,HIGH);
+  }
+ 
+  //EXERCICIO 3 
+  int numere = 4;
+  int resultado2 = numere%2;
+  
+  if(resultado2 == 0){
+    digitalWrite(9,HIGH);
+  }
+   
+  
+}
+
+
